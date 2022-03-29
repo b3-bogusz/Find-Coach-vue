72ddbb8f32384cdcd991a166c9eb49c31b5f3b77
Patryk Bogusz <b3.bogusz@gmail.com>
2022-03-29 20:25:08 +0200

    add filtering requests for the active coach

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,9 @@
 export default {
-    getRequestsList(state) {
-        return state.requests;
+    getRequestsList(state, _, _2, rootGetters) {
+        const coachId = rootGetters.userId;
+        return state.requests.filter(req => req.coachId === coachId);
     },
-    hasRequests(state) {
-        return state.requests && state.requests.length > 0;
+    hasRequests(_, getters) {
+        return getters.getRequestsList && getters.getRequestsList.length > 0;
     }
 }

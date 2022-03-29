e8cc2e65d595926ea3cddfdeccdf0aafdc7fb6e7
Patryk Bogusz <b3.bogusz@gmail.com>
2022-03-29 20:12:14 +0200

    add RequestItem and output requests in ui

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,16 +1,55 @@
 <template>
   <section>
-    REQUESTS
+    <BaseCard>
+      <header>
+        <h2>Requests Recieved</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <RequestItem
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        >
+        </RequestItem>
+      </ul>
+      <h3 v-else>You haven't received any requests yet...</h3>
+    </BaseCard>
   </section>
 </template>
 
 <script>
+import BaseCard from '../../components/ui/BaseCard';
+import RequestItem from '../../components/requests/RequestItem';
 export default {
-
+  components: {
+    RequestItem,
+    BaseCard
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/getRequestsList']
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests']
+    }
+  }
 }
 </script>
 
+<style scoped>
+header {
+  text-align: center;
+}
 
-<style>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
 
+h3 {
+  text-align: center;
+}
 </style>
--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,8 @@
-export default {}
+export default {
+    getRequestsList(state) {
+        return state.requests;
+    },
+    hasRequests(state) {
+        return state.requests && state.requests.length > 0;
+    }
+}

00530c39aa9bf8f0d8b6d5bc40eaea9c88067e1b
Patryk Bogusz <b3.bogusz@gmail.com>
2022-03-30 12:03:42 +0200

    add Page Not Found component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,16 +1,15 @@
 <template>
-  <main>
-
-  </main>
+  <section>
+    <BaseCard>
+      <h2>Page Not Found</h2>
+      <p>This page could not be found - please check out all our <router-link to="/coaches">coaches</router-link></p>
+    </BaseCard>
+  </section>
 </template>
 
 <script>
+import BaseCard from '../components/ui/BaseCard';
 export default {
-
+  components: { BaseCard }
 }
 </script>
-
-
-<style>
-
-</style>

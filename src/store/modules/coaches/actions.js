85033803b7e5798c08a7e673ec2f8e70eb84e7f2
Patryk Bogusz <b3.bogusz@gmail.com>
2022-03-31 15:14:01 +0200

    add authentication token to outgoing requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,7 +9,9 @@ export default {
             hourlyRate: data.rate
         };
 
-        const response = await fetch(`https://findcoach-23390-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+        const token = context.rootGetters.token;
+
+        const response = await fetch(`https://findcoach-23390-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=` + token, {
             method: 'PUT',
             body: JSON.stringify(coachData)
         });
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -25,7 +25,8 @@ export default {
     },
     async fetchRequests(context) {
         const coachId = context.rootGetters.userId;
-        const response = await fetch(`https://findcoach-23390-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`)
+        const token = context.rootGetters.token;
+        const response = await fetch(`https://findcoach-23390-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json?auth=` + token)
         const responseData = await response.json();
 
         if (!response.ok) {
